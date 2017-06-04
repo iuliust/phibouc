af24f01f0d6240f9ace66ec69e7c45b2c6356906
Julien Teyteau <iuliust@gmail.com>
2017-06-05 00:41:26 +0200

    feat(quotes) add delete mutation

--- src/app/containers/candidat-presentation/candidat-presentation.component.ts
+++ src/app/containers/candidat-presentation/candidat-presentation.component.ts
@@ -4,6 +4,11 @@ import gql from 'graphql-tag';
 
 const quotes = gql`
   query {
+    authors {
+      id
+      firstName
+      lastName
+    }
     quotes {
       id
       text
@@ -15,7 +20,28 @@ const quotes = gql`
   }
 `;
 
+const createQuote = gql`
+  mutation createQuote($input: QuoteInput!) {
+    createQuote(
+      input: $input
+    ) {
+      id
+      text
+      author {
+        id
+        firstName
+        lastName
+      }
+    }
+  }
+`;
+
 interface QuotesResponse {
+  authors: {
+    id: string;
+    firstName: string;
+    lastName: string;
+  }[];
   quotes: {
     id: string;
     text: string;
@@ -33,7 +59,11 @@ interface QuotesResponse {
 })
 export class CandidatPresentationComponent implements OnInit {
   quotesList: any[];
-  loading: boolean;
+  authorsList: any[];
+  newQuote = {
+    text: '',
+    author: null,
+  };
 
   constructor(private apollo: Apollo) { }
 
@@ -42,8 +72,52 @@ export class CandidatPresentationComponent implements OnInit {
       query: quotes
     })
     .subscribe(({data}) => {
-      const test = data as QuotesResponse;
-      this.quotesList = test.quotes;
+      const cast = data as QuotesResponse;
+      this.quotesList = cast.quotes;
+      this.authorsList = cast.authors;
+    });
+  }
+
+  addQuote(text: string, author: string) {
+    this.apollo.mutate({
+        mutation: createQuote,
+        variables: {
+          input: {
+            text,
+            author,
+          },
+        },
+      })
+      .subscribe(({data}) => {
+        const cast = data as any;
+        this.quotesList = this.quotesList.concat([cast.createQuote]);
+        this.newQuote = {
+          text: '',
+          author: null,
+        };
+      }, (err) => {
+        console.error(err);
+      });
+  }
+
+  deleteQuote(id: string) {
+    this.apollo.mutate({
+      mutation: gql`
+        mutation deleteQuote($id: ID!) {
+          deleteQuote(
+            id: $id
+          ) {
+            ok
+          }
+        }
+      `,
+      variables: {id}
+    })
+    .subscribe(({data}) => {
+      const cast = data as any;
+      if (cast.deleteQuote.ok) {
+        this.quotesList = this.quotesList.filter(el => el.id !== id);
+      }
     });
   }
 
